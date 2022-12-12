8d079322775e91b8572b1b3935b6c74858106a49
arwrdi <arwinrnrdi@gmail.com>
2022-12-12 19:24:03 +0700

    DOM about

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function toggleDetail(e){
+    const target = $(e.target)
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+
+    $(detail).slideToggle()
+    
+    console.log($(item).children(1))
+}
