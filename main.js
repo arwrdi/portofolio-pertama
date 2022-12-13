f1f6532bbf0b4c291bb5025f227d0d764aa0a8c2
arwrdi <arwinrnrdi@gmail.com>
2022-12-13 19:51:14 +0700

    contact validation

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ function toggleDetail(e){
     
     console.log($(item).children(1))
 }
+
+function onFormSubmit(e){
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const messages = $("#inp_messages")
+    // console.log($(subject).val())
+    if(!$(email).val()){
+        alert("Email is required")
+        
+    } else if(!$(subject).val()){
+        alert("subject is required")
+    }
+    else if (!$(messages)){
+        alert("messages is required")
+    } else{
+        alert("form submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(messages).val("")
+
+    }
+}
